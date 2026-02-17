98b1123ea1ea3bbb1ac2541ec2921844034c5d5c
nothing320 <don073816@gmail.com>
2026-02-17 19:14:40 +0500

    Update No_Gold_Zone.user.js

--- No_Gold_Zone.user.js
+++ No_Gold_Zone.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Usman Pro - No Gold (XAU/GOLD) Block
 // @namespace    http://tampermonkey.net/
-// @version      1.1
-// @description  Blocks XAU and GOLD typing in search bar
+// @version      1.2
+// @description  Blocks XAU, GOLD, GOL, and GO typing in search bar
 // @author       Usman
 // @match        https://mt5real35.exwebterm.com/terminal?*
 // @updateURL    https://raw.githubusercontent.com/nothing320/BRM-system/main/MT5_No_Gold_Zone.user.js
@@ -14,18 +14,26 @@
 (function() {
     'use strict';
 
+    // Search bar ka rasta
     const searchPath = 'body > div.layout.svelte-vce879 > div.right-panel.svelte-1gxqakg > div.layout.svelte-b7mtja.left > label.search.svelte-1mvzp7f > input.svelte-1mvzp7f';
 
     function blockGold() {
         const searchInput = document.querySelector(searchPath);
         if (searchInput) {
             const val = searchInput.value.toUpperCase();
-            if (val.includes('XAU') || val.includes('GOLD')) {
+            
+            // Sab keywords check karega: XAU, GOLD, GOL, GO
+            if (val.includes('XAU') || val.includes('GOLD') || val.includes('GOL') || val.includes('GO')) {
                 searchInput.value = ""; 
+                
+                // Website ko batane ke liye ke input change hua hai
                 searchInput.dispatchEvent(new Event('input', { bubbles: true }));
+                
+                console.log("SURGICAL LOCK: GOLD related keywords are blocked!");
             }
         }
     }
 
+    // Har 100 milliseconds baad check karega
     setInterval(blockGold, 100);
 })();
