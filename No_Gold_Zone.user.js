0b6f7d5a8085fbbb705e80596321c79f3053270c
nothing320 <don073816@gmail.com>
2026-02-17 18:57:47 +0500

    Update No_Gold_Zone.user.js

--- No_Gold_Zone.user.js
+++ No_Gold_Zone.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Usman Pro - No Gold (XAU) Block
+// @name         Usman Pro - No Gold (XAU/GOLD) Block
 // @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  Blocks XAU typing in search bar
+// @version      1.1
+// @description  Blocks XAU and GOLD typing in search bar
 // @author       Usman
 // @match        https://mt5real35.exwebterm.com/terminal?*
 // @updateURL    https://raw.githubusercontent.com/nothing320/BRM-system/main/MT5_No_Gold_Zone.user.js
@@ -16,13 +16,16 @@
 
     const searchPath = 'body > div.layout.svelte-vce879 > div.right-panel.svelte-1gxqakg > div.layout.svelte-b7mtja.left > label.search.svelte-1mvzp7f > input.svelte-1mvzp7f';
 
-    function blockXAU() {
+    function blockGold() {
         const searchInput = document.querySelector(searchPath);
-        if (searchInput && searchInput.value.toUpperCase().includes('XAU')) {
-            searchInput.value = ""; 
-            searchInput.dispatchEvent(new Event('input', { bubbles: true }));
+        if (searchInput) {
+            const val = searchInput.value.toUpperCase();
+            if (val.includes('XAU') || val.includes('GOLD')) {
+                searchInput.value = ""; 
+                searchInput.dispatchEvent(new Event('input', { bubbles: true }));
+            }
         }
     }
 
-    setInterval(blockXAU, 100);
+    setInterval(blockGold, 100);
 })();
