7e2fd8e1f0569fee5716314acf407e74b1d54f09
nothing320 <don073816@gmail.com>
2026-02-18 17:58:34 +0500

    Update No_Gold_Zone.user.js

--- No_Gold_Zone.user.js
+++ No_Gold_Zone.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Usman Pro - No Gold (XAU/GOLD) Block
 // @namespace    http://tampermonkey.net/
-// @version      1.3
-// @description  Blocks XAU, GOLD, GOL, GO, XU, and AU typing in MT5 search bar
+// @version      1.2
+// @description  Blocks XAU, GOLD, GOL, and GO typing in search bar
 // @author       Usman
 // @match        https://mt5real35.exwebterm.com/terminal?*
 // @updateURL    https://raw.githubusercontent.com/nothing320/BRM-system/main/MT5_No_Gold_Zone.user.js
@@ -14,7 +14,7 @@
 (function() {
     'use strict';
 
-    // MT5 Search bar ka specific rasta
+    // Search bar ka rasta
     const searchPath = 'body > div.layout.svelte-vce879 > div.right-panel.svelte-1gxqakg > div.layout.svelte-b7mtja.left > label.search.svelte-1mvzp7f > input.svelte-1mvzp7f';
 
     function blockGold() {
@@ -22,25 +22,18 @@
         if (searchInput) {
             const val = searchInput.value.toUpperCase();
             
-            // Sab keywords: XAU, GOLD, GOL, GO, XU, AU
-            if (val.includes('XAU') || 
-                val.includes('GOLD') || 
-                val.includes('GOL') || 
-                val.includes('GO') || 
-                val.includes('XU') || 
-                val.includes('XA')) {
-                
+            // Sab keywords check karega: XAU, GOLD, GOL, GO
+            if (val.includes('XAU') || val.includes('GOLD') || val.includes('X') || val.includes('G')) {
                 searchInput.value = ""; 
                 
-                // MT5 ko update karne ke liye double event trigger
+                // Website ko batane ke liye ke input change hua hai
                 searchInput.dispatchEvent(new Event('input', { bubbles: true }));
-                searchInput.dispatchEvent(new Event('change', { bubbles: true }));
                 
-                console.log("SURGICAL LOCK: Gold/XU keywords are blocked!");
+                console.log("SURGICAL LOCK: GOLD related keywords are blocked!");
             }
         }
     }
 
-    // Har 100 milliseconds baad monitor karega
+    // Har 100 milliseconds baad check karega
     setInterval(blockGold, 100);
 })();
