4f6eb667fa74a06d3dfc6c7a5aa18bdaf01949b7
nothing320 <don073816@gmail.com>
2026-02-19 13:09:15 +0500

    Create Link Test.user.js

---
+++ Link Test.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Usman Pro - Link Test
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Simple update test
+// @author       Usman
+// @match        *://*.metatrader5.com/*
+// @match        *://*.exwebterm.com/*
+// @updateURL    https://raw.githubusercontent.com/nothing320/BRM-system/main/MT5_Icon_Hider.user.js
+// @downloadURL  https://raw.githubusercontent.com/nothing320/BRM-system/main/MT5_Icon_Hider.user.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    alert("Usman Bhai: Version 1.0 Active!");
+})();
